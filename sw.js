07f61262b1fb3b7f915a6ccd2a3aaa27adf76cf0
KartiX <kartik421kartik@gmail.com>
2026-09-18 08:47:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,47 @@
-const CACHE_NAME = 'edunexus-live-v9';
+const CACHE_NAME = 'edunexus-live-v10';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://i.ibb.co/R4bq8sqB/KXEDGE.png' // Yahan direct image link (i.ibb.co) daal diya!
+  './',
+  './index.html',
+  './manifest.json',
+  'https://i.ibb.co/R4bq8sqB/KXEDGE.png', // 512 wala link
+  'https://i.ibb.co/ccXF7gCj/kx1.png'    // 192 wala link bhi aa gaya
 ];
 
-
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('🧹 KX Core: Old cache cleared ->', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('🧹 KX Core: Old cache cleared ->', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-  if (!event.request.url.startsWith('http')) return;
+  if (event.request.method !== 'GET') return;
+  if (!event.request.url.startsWith('http')) return;
 
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        if (response && response.status === 200) {
-          const resClone = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response && response.status === 200) {
+          const resClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
